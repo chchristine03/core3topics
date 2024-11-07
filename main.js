0bae04f8f28bd920d463a681ac3caf1dd2efcccf
chchristine03 <chchristine03@gmail.com>
2024-11-07 17:23:11 -0500

    comments

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const divElement = document.querySelector('.d13'); // Replace 'div' with the ID or class of your actual div element
+const divElement = document.querySelector('.d13'); 
 
 divElement.addEventListener('click', function() {
-  window.location.href = 'https://chchristine03.github.io/core3topics/story2/index.html'; // Replace 'https://www.example.com' with the URL of your desired page
+  window.location.href = 'https://chchristine03.github.io/core3topics/story2/index.html';
 });
