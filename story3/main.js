162cb697b1ad23ece00edd8d02023696ee346774
chchristine03 <chchristine03@gmail.com>
2024-12-03 18:57:58 -0500

    another

--- story3/main.js
+++ story3/main.js
@@ -29,3 +29,11 @@
      alert("Please select at least one ticket.");
    }
  });
+
+ 
+
+const divElement = document.querySelector('.backcandle'); 
+
+divElement.addEventListener('click', function() {
+  window.location.href = 'https://chchristine03.github.io/core3topics/index.html';
+});
